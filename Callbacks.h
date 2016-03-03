0ca5db183a3008afc647b81e1e7bf8f88fcb6b9e
xie hui <61581090@qq.com>
2016-03-02 21:50:16 -0800

    add down_pointer_cast in Callbacks.h

--- Callbacks.h
+++ Callbacks.h
@@ -8,6 +8,22 @@
 
 //All client visible callbacks go here
 
+//Adapted from google-protobuf common.h
+template<typename To, typename From>
+inline ::boost::shared_ptr<To> down_pointer_cast(const ::boost::shared_ptr<From>& f)
+{
+	if (false)
+	{
+		implicit_cast<From*, To*>(0);
+	}
+
+#ifndef NDEBUG
+	assert(f == NULL || dynamic_cast<To*>(get_pointer(f)) != NULL);
+#endif
+	return ::boost::static_pointer_cast<To>(f);
+}
+
+
 class Buffer;
 class TcpConnection;
 typedef boost::shared_ptr<TcpConnection> TcpConnectionPtr;
